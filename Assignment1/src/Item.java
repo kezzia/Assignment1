0561534f2240238109ecb7c82aeec99d0ba1f1c3
undeadVivisector <mvandertramps@gmail.com>
2016-01-28 07:57:37 -0500

    First commit

---
+++ Assignment1/src/Item.java
@@ -0,0 +1,13 @@
+
+public class Item {
+	String name;
+	String description;
+	float price;
+	String warrantyPeriod;
+	String supplier;
+	String url;
+	String location;
+	int numberInStock;
+	int weight;
+
+}
